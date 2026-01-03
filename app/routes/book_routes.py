82637f78c2113576c6c66ad3f1a4c45f6ace6b5e
Qiusi Agre <qiusi.agre@icloud.com>
2026-01-03 15:10:01 -0500

    Refactor get_one_book and validate_book functions for clarity and re-enable book retrieval

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -8,7 +8,7 @@ books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 def get_all_books():
     query = db.select(Book).order_by(Book.id)
     books = db.session.scalars(query)
-    
+
     books_response = []
     for book in books:
         books_response.append(
@@ -20,29 +20,31 @@ def get_all_books():
         )
     return books_response
 
-# @books_bp.get("/<book_id>")
-# def get_one_book(book_id):
-#     book = validate_book(book_id)
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
 
-#     return {
-#         "id": book.id,
-#         "title": book.title,
-#         "description": book.description,
-#     }
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description,
+    }
 
-# def validate_book(book_id):
-#     try:
-#         book_id = int(book_id)
-#     except:
-#         response = {"message": f"book {book_id} invalid"}
-#         abort(make_response(response, 400))
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} invalid"}
+        abort(make_response(response, 400))
 
-#     for book in books:
-#         if book.id == book_id:
-#             return book
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
 
-#     response = {"message": f"book {book_id} not found"}
-#     abort(make_response(response, 404))
+    if not book:
+        response = {"message": f"book {book_id} not found"}
+        abort(make_response(response, 404))
+       
+    return book
 
 @books_bp.post("")
 def create_book():
