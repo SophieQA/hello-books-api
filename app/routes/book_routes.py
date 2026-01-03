fb4370f701156677aa807e3c9c16572667a84c34
Qiusi Agre <qiusi.agre@icloud.com>
2026-01-03 14:23:29 -0500

    Refactor get_all_books function to improve readability and maintainability

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -4,18 +4,21 @@ from app.db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-# @books_bp.get("")
-# def get_all_books():
-#     books_response = []
-#     for book in books:
-#         books_response.append(
-#             {
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             }
-#         )
-#     return books_response
+@books_bp.get("")
+def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+    
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return books_response
 
 # @books_bp.get("/<book_id>")
 # def get_one_book(book_id):
